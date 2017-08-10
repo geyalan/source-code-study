3dac3e2a31b825c51375c9a80505737ca6b3af87
geyalan <490312867@qq.com>
2017-08-10 15:31:59 +0800

    add

--- zepto源码学习/zepto-ever.js
+++ zepto源码学习/zepto-ever.js
@@ -119,45 +119,46 @@ $('some css selector').html('set contents').css('set styles');*/
 /******************************************zepto-3********************************************************************/
 
 /******************************************zepto-4********************************************************************/
-function $(_){
-  if(typeof _ == 'function') $.dom.forEach(_)
-    else { 
-      $._   = _;
-      $.dom = [].slice.apply(document.querySelectorAll(_));
-    }
-  return $.fn;
-}
+(function(slice){
+	var $ = function(_){
+	  if(typeof _ == 'function') $.dom.forEach(_)
+	    else { 
+	      $._   = _;
+	      $.dom = slice.call(document.querySelectorAll(_));
+	    }
+	  return $.fn;
+	}
 
-$.fn = {
-  get: function(idx){ return idx === undefined ? $.dom : $.dom[idx] },
-  html: function(html){
-    return $(function(el){ el.innerHTML = html });
-  },
-  append: function(html){
-    return $(function(el){ el.insertAdjacentHTML('beforeEnd',html) });
-  },
-  prepend: function(html){
-    return $(function(el){ el.insertAdjacentHTML('afterBegin',html) });
-  },
-  css: function(style){
-    return $(function(el){ el.style.cssText += ';'+style });
-  },
-  anim: function(transform, opacity, dur){
-    return $.fn.css('-webkit-transition:all '+(dur||0.5)+'s;'+
-      '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
-  },
-  live: function(event, callback){
-    var selector = $._;
-    document.body.addEventListener(event, function(event){
-      var target = event.target, nodes = [].slice.apply(document.querySelectorAll(selector));
-      while(target && nodes.indexOf(target)<0) target = target.parentNode;
-      if(target && !(target===document)) callback.call(target, event);
-    }, false);
-    return $.fn;
-  }
-};
+	$.fn = {
+	  get: function(idx){ return idx === undefined ? $.dom : $.dom[idx] },
+	  html: function(html){
+	    return $(function(el){ el.innerHTML = html });
+	  },
+	  append: function(html){
+	    return $(function(el){ el.insertAdjacentHTML('beforeEnd',html) });
+	  },
+	  prepend: function(html){
+	    return $(function(el){ el.insertAdjacentHTML('afterBegin',html) });
+	  },
+	  css: function(style){
+	    return $(function(el){ el.style.cssText += ';'+style });
+	  },
+	  anim: function(transform, opacity, dur){
+	    return $.fn.css('-webkit-transition:all '+(dur||0.5)+'s;'+
+	      '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
+	  },
+	  live: function(event, callback){
+	    var selector = $._;
+	    document.body.addEventListener(event, function(event){
+	      var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
+	      while(target && nodes.indexOf(target)<0) target = target.parentNode;
+	      if(target && !(target===document)) callback.call(target, event);
+	    }, false);
+	    return $.fn;
+	  }
+	};
 
-(function(){
+
   function ajax(method, url, success){
     var r = new XMLHttpRequest();
     r.onreadystatechange = function(){
@@ -173,5 +174,6 @@ $.fn = {
   $.getJSON = function(url, success){ 
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
-})();
+  this.$ = $;
+})([].slice);
 /******************************************zepto-4********************************************************************/
