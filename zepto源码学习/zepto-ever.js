4c7800d95c000e9f4eae7f84714d3ec29d7f1812
geyalan <490312867@qq.com>
2017-08-23 09:51:47 +0800

    add

--- zepto源码学习/zepto-ever.js
+++ zepto源码学习/zepto-ever.js
@@ -175,51 +175,155 @@ $('some css selector').html('set contents').css('set styles');*/
 /******************************************zepto-4********************************************************************/
 /******************************************zepto-5********************************************************************/
 
-var $ = (function(){
+// var $ = (function(d){
+// 	var slice = [].slice,k,
+// 		ADJ_OPS = {append:'beforeEnd',prepend:'afterBegin',before:'beforeBegin',after:'afterEnd'};
+	
+	
+// 	function $(_){
+// 		function fn(_){
+// 			return arguments.callee.dom.forEach(_),arguments.callee;
+// 		}
+// 		// fn.dom = slice.call(document.querySelectorAll(fn.selector=_));
+// 		fn.dom = _ instanceof Element?[_]:slice.call(d.querySelectorAll(fn.selector=_))
+// 		for(k in $.fn) {
+// 			fn[k] = $.fn[k]
+// 		}
+// 		return fn
+// 	}
+
+// 	function classRE(name) {return new RegExp("(^|\\s)"+name+"(\\s|$)")}
+
+// 	$.fn = {
+// 	  get: function(idx){ 
+// 	  	return idx === void 0 ? $.dom : $.dom[idx] 
+// 	  },
+// 	  html: function(html){
+// 	  	if(html=== void 0) return this.dom[0].innerHTML || void 0;
+// 	    return this(function(el){ el.innerHTML = html });
+// 	  },
+// 	  attr:function(name,value){
+// 	  		if(value== void 0) return this.dom[0].getAttribute(name) || void 0;
+// 	  		return this(function(el){el.setAttribute(name,value)});
+// 	  },
+// 	  css: function(style){
+// 	    return this(function(el){ el.style.cssText += ';'+style });
+// 	  },
+// 	  index:function(target){
+// 	  	return [].inexOf.call(this.dom,$(target).get(0))
+// 	  },
+// 	  anim: function(transform, opacity, dur){
+// 	    return this.css('-webkit-transition:all '+(dur||0.5)+'s;'+
+// 	      '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
+// 	  },
+// 	  // live: function(event, callback){
+// 	  //   var selector = this.selector;
+// 	  //   document.body.addEventListener(event, function(event){
+// 	  //     var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
+// 	  //     while(target && nodes.indexOf(target)<0) target = target.parentNode;
+// 	  //     if(target && !(target===document)) callback.call(target, event);
+// 	  //   }, false);
+// 	  //   return this;
+// 	  // },
+// 	  delegate:function(selector,event,callback){
+// 	  	return this(function(el){
+// 	  		el.addEventListener(event,function(event){
+// 	  			var target = event.target,node = slice.call(el.querySelectorAll(selector));
+// 	  			while(target && nodes.indexOf(target)<0) target = target.parentNode;
+// 	  			if(target && !(target===el)) callback.call(target,event);
+// 	  		},false)
+// 	  	})
+// 	  },
+// 	  addClass:function(name){
+// 	  	return this(function(el){
+// 	  		!classRE(name).test(el.className) && (el.className += (el.className?' ':'')+name);
+// 	  	})
+// 	  },
+// 	  removeClass:function(name){
+// 	  	return this(function(el){
+// 	  		el.className = el.className.replace(classRE(name),' ').replace(/^\s+|\s+$/,'');
+// 	  	})
+// 	  },
+// 	};
+
+// 	for(k in ADJ_OPS)
+//     $.fn[k] = (function(op){ 
+//       return function(html){ return this(function(el){ el.insertAdjacentHTML(op,html) }) };
+//     })(ADJ_OPS[k]);
+
+//   function ajax(method, url, success){
+//     var r = new XMLHttpRequest();
+//     r.onreadystatechange = function(){
+//       if(r.readyState==4 && (r.status==200 || r.status==0))
+//         success(r.responseText);
+//     };
+//     r.open(method,url,true);
+//     r.send(null);
+//   }
+  
+//   $.get = function(url, success){ ajax('GET', url, success); };
+//   $.post = function(url, success){ ajax('POST', url, success); };
+//   $.getJSON = function(url, success){ 
+//     $.get(url, function(json){ success(JSON.parse(json)) });
+//   };
+//   return $
+// })(document);
+/******************************************zepto-5********************************************************************/
+/******************************************zepto-5********************************************************************/
+
+var $ = (function(d){
 	var slice = [].slice,k,
 		ADJ_OPS = {append:'beforeEnd',prepend:'afterBegin',before:'beforeBegin',after:'afterEnd'};
 	
 	
 	function $(_){
 		function fn(_){
-			return arguments.callee.dom.forEach(_),arguments.callee;
+			return fn.dom.forEach(_),fn;
 		}
-		// fn.dom = slice.call(document.querySelectorAll(fn.selector=_));
-		fn.dom = _ instanceof Element?[_]:slice.call(document.querySelectorAll(fn.selector=_))
+		fn.dom = _ instanceof Element?[_]:slice.call(d.querySelectorAll(fn.selector=_))
 		for(k in $.fn) {
 			fn[k] = $.fn[k]
 		}
 		return fn
 	}
 
+	function classRE(name) {return new RegExp("(^|\\s)"+name+"(\\s|$)")}
+
 	$.fn = {
 	  get: function(idx){ 
-	  	return idx === undefined ? $.dom : $.dom[idx] 
+	  	return idx === void 0 ? $.dom : $.dom[idx] 
+	  },
+	  remove:function(){
+	  	return this(function(el){el.parentNode.removeChild(el)})
+	  },
+	  each:function(callback){
+	  	return this(function(e){callback(e)})
 	  },
 	  html: function(html){
-	  	if(html===undefined) return this.dom[0].innerHTML || undefined;
-	    return this(function(el){ el.innerHTML = html });
+	  	return html === void 0 ?(this.dom.length>0?this.dom[0].innerHTML:null) : this(function(el){el.innerHTML = html})
 	  },
-	  attr:function(name,value){
-	  		if(value==undefined) return this.dom[0].getAttribute(name) || undefined;
-	  		return this(function(el){el.setAttribute(name,value)});
-	  },
+	  attr: function(name,value){
+	      return (typeof name == 'string' && value === void 0) ? (this.dom.length>0 ? this.dom[0].getAttribute(name) || undefined : null) :
+	        this(function(el){ 
+	          if (typeof name == 'object') for(k in name) el.setAttribute(k, name[k])
+	          else el.setAttribute(name,value);
+	        });
+      },
 	  css: function(style){
 	    return this(function(el){ el.style.cssText += ';'+style });
+	  },
+	  index:function(target){
+	  	return [].inexOf.call(this.dom,$(target).get(0))
 	  },
 	  anim: function(transform, opacity, dur){
 	    return this.css('-webkit-transition:all '+(dur||0.5)+'s;'+
 	      '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
 	  },
-	  // live: function(event, callback){
-	  //   var selector = this.selector;
-	  //   document.body.addEventListener(event, function(event){
-	  //     var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
-	  //     while(target && nodes.indexOf(target)<0) target = target.parentNode;
-	  //     if(target && !(target===document)) callback.call(target, event);
-	  //   }, false);
-	  //   return this;
-	  // },
+	  bind:function(event,callback){
+	  	return this(function(el){
+	  		event.splite(/\s/).forEach(function(event){el.addEventListener(event,callback,false)});
+	  	})
+	  },
 	  delegate:function(selector,event,callback){
 	  	return this(function(el){
 	  		el.addEventListener(event,function(event){
@@ -229,11 +333,23 @@ var $ = (function(){
 	  		},false)
 	  	})
 	  },
+	  addClass:function(name){
+	  	return this(function(el){
+	  		!classRE(name).test(el.className) && (el.className += (el.className?' ':'')+name);
+	  	})
+	  },
+	  removeClass:function(name){
+	  	return this(function(el){
+	  		el.className = el.className.replace(classRE(name),' ').replace(/^\s+|\s+$/,'');
+	  	})
+	  },
 	};
 
 	for(k in ADJ_OPS)
     $.fn[k] = (function(op){ 
-      return function(html){ return this(function(el){ el.insertAdjacentHTML(op,html) }) };
+      return function(html){ return this(function(el){ 
+      	el['insertAdjacent' + (html instanceof Element ? 'Element' : 'HTML')](op,html)
+      }) };
     })(ADJ_OPS[k]);
 
   function ajax(method, url, success){
@@ -252,5 +368,6 @@ var $ = (function(){
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
   return $
-})();
+})(document);
+
 /******************************************zepto-5********************************************************************/
