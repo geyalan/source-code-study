c3cf26a2e7e51eb45b32161530be610a9d56f50b
geyalan <490312867@qq.com>
2017-08-28 11:45:21 +0800

    add

--- zepto源码学习/zepto-ever.js
+++ zepto源码学习/zepto-ever.js
@@ -372,12 +372,12 @@ var $ = (function(d){
       }) };
     })(ADJ_OPS[k]);
 
-    ['swipeLeft', 'swipeRight', 'doubleTap', 'tap'].forEach(function(m){
+    ['swipe', 'doubleTap', 'tap'].forEach(function(m){
     	$.fn[m] = function(callback){ return this.bind(m, callback) }
   	});
 
 	  function dispatch(event, target) {
-	    var e = document.createEvent('Events');
+	    var e = d.createEvent('Events');
 	    e.initEvent(event, true, false);
 	    target.dispatchEvent(e);
 	  }
@@ -398,8 +398,8 @@ var $ = (function(d){
 	  d.ontouchend = function(e) {
 	    var t = e.target;
 	    if (t.x2 > 0) {
-	      t.x1 - t.x2 > 30 && dispatch('swipeLeft', t);
-	      t.x1 - t.x2 < -30 && dispatch('swipeRight', t);
+	      t.x1 - t.x2 > 30 && dispatch('swipe', t);
+	      t.x1 - t.x2 < -30 && dispatch('swipe', t);
 	      t.x1 = t.x2 = t.last = 0;
 	    } else if (t.last != 0) {
 	      dispatch('tap', t);
